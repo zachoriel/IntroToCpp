2192734ff8f46c34fa35990f0d4ea14f8fe4e128
s179065 <s179065@SEA-1A7525>
2017-08-14 12:46:03 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello AIE";
+
+	std::cout << "Track 1" << std::endl;
+	std::cout << "Track 2" << std::endl;
+	std::cout << "Track 3" << std::endl;
+	std::cout << "Track 4" << std::endl;
+	std::cout << "Track 5" << std::endl;
+
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+
+	system("pause");
+
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl;
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+	
+	system("pause");
+
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl;
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+	
+	system("pause");
+
+}
